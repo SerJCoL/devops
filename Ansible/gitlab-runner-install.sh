c7b94d8b427303feb3511f366d19526454516722
SerJCoL <113114305+SerJCoL@users.noreply.github.com>
2024-11-18 09:05:32 +0300

    Delete Ansible/gitlab-runner-install.sh

--- Ansible/gitlab-runner-install.sh
+++
@@ -1,5 +0,0 @@
-# Add repository  GitLab Runner
-curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | sudo bash
-
-# Install GitLab Runner
-sudo apt-get install -y gitlab-runner
